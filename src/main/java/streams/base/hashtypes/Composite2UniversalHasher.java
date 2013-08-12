5353634b1a33660d1feb76093cad6cb41e5d5919
Ananth Kalyan Chakravarthy Gundabattula <chakri.ananth@gmail.com>
2013-08-12 21:57:20 +1000

    Implement basic version of composite asher

---
+++ src/main/java/streams/base/hashtypes/Composite2UniversalHasher.java
@@ -0,0 +1,11 @@
+package streams.base.hashtypes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ananthgundabattula
+ * Date: 8/12/13
+ * Time: 9:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Composite2UniversalHasher {
+}
---
+++ src/main/java/streams/base/hashtypes/CompositeHasherFactory.java
@@ -0,0 +1,11 @@
+package streams.base.hashtypes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ananthgundabattula
+ * Date: 8/12/13
+ * Time: 8:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CompositeHasherFactory {
+}
